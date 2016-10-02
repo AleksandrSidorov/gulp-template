a4aa841830bfc1b58c175ac5f554e479dc6d906a
Aleksandr Sidorov <shtil80@gmail.com>
2016-10-02 22:37:51 +0700

    Pug support. CSS Iport added.

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,24 @@ var sourcemaps = require('gulp-sourcemaps');
 gulp.task('css', function () {
   var postcss = require('gulp-postcss');
   var cssnext = require('postcss-cssnext');
+  var atImport = require('postcss-import');
 
-  var processors = [ cssnext ];
+  var processors = [ atImport, cssnext ];
 
   return gulp.src('src/**/*.css')
     .pipe( sourcemaps.init() )
     .pipe( postcss(processors) )
     .pipe( sourcemaps.write('.') )
     .pipe( gulp.dest('build/') );
+});
+
+gulp.task('pug', function () {
+  var pug = require('gulp-pug');
+
+  return gulp.src('src/**/*.pug')
+    .pipe( sourcemaps.init() )
+    .pipe( pug() )
+    .pipe( sourcemaps.write('.') )
+    .pipe( gulp.dest('build/') );
+
 });

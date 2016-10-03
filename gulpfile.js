f3747b2b7c56568e25c6d3f4750e8dccb1d99fba
Aleksandr Sidorov <shtil80@gmail.com>
2016-10-03 18:35:47 +0700

    Browsersync, cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,52 @@
+'use strict';
+
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
+var newer = require('gulp-newer');
+var concat = require('gulp-concat');
+var cssnano = require('gulp-cssnano');
+var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
 
-gulp.task('css', function () {
-  var postcss = require('gulp-postcss');
-  var cssnext = require('postcss-cssnext');
-  var atImport = require('postcss-import');
+const reload = browserSync.reload;
 
-  var processors = [ atImport, cssnext ];
+gulp.task('css', () => {
+    var postcss = require('gulp-postcss');
+    var cssnext = require('postcss-cssnext');
+    var atImport = require('postcss-import');
 
-  return gulp.src('src/**/*.css')
-    .pipe( sourcemaps.init() )
-    .pipe( postcss(processors) )
-    .pipe( sourcemaps.write('.') )
-    .pipe( gulp.dest('build/') );
+    var processors = [ atImport, cssnext ];
+
+    return gulp.src('src/**/*.css')
+        .pipe( newer('build/css') )
+        .pipe( sourcemaps.init() )
+        .pipe( postcss(processors) )
+        .pipe( concat('style.css') )
+        .pipe( gulp.dest('build/css') )
+        .pipe( cssnano() )
+        .pipe( rename({extname: ".min.css"}) )
+        .pipe( sourcemaps.write('../sourcemaps') )
+        .pipe( gulp.dest('build/css') );
 });
 
-gulp.task('pug', function () {
-  var pug = require('gulp-pug');
+gulp.task('pug', () => {
+    var pug = require('gulp-pug');
 
-  return gulp.src('src/**/*.pug')
-    .pipe( sourcemaps.init() )
-    .pipe( pug() )
-    .pipe( sourcemaps.write('.') )
-    .pipe( gulp.dest('build/') );
+    return gulp.src('src/**/*.pug')
+        .pipe( newer('build/**/*.html') )
+        .pipe( sourcemaps.init() )
+        .pipe( pug({ pretty: true }) )
+        .pipe( sourcemaps.write('./sourcemaps') )
+        .pipe( gulp.dest('build/') );
+});
 
+gulp.task('serve', ['css', 'pug'], () => {
+    browserSync.init({
+        server: {
+            baseDir: './build/'
+        }
+    });
+
+    gulp.watch('src/**/*.css', ['css', reload]);
+    gulp.watch('src/**/*.pug', ['pug', reload]);
 });
